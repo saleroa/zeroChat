a134df79f444d6aaecee22864c2d810b2fca2c45
saleroa <2985299550@qq.com>
2024-06-09 12:41:48 +0800

    user server fix done

--- apps/user/rpc/internal/svc/servicecontext.go
+++ apps/user/rpc/internal/svc/servicecontext.go
@@ -1,22 +1,38 @@
 package svc
 
 import (
+	"time"
 	"zeroChat/apps/user/models"
 	"zeroChat/apps/user/rpc/internal/config"
+	"zeroChat/pkg/constants"
+	"zeroChat/pkg/ctxdata"
 
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
 	Config    config.Config
 	UserModel models.UsersModel
+	*redis.Redis
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	sqlConn := sqlx.NewMysql(c.Mysql.DataSource)
 	return &ServiceContext{
-		Config: c,
-
+		Config:    c,
+		Redis:     redis.MustNewRedis(c.Redisx),
 		UserModel: models.NewUsersModel(sqlConn, c.Cache),
 	}
+}
+
+func (svc *ServiceContext) SetRootToken() error {
+	// 生成jwt
+	systemToken, err := ctxdata.GetJwtToken(svc.Config.Jwt.AccessSecret, time.Now().Unix(), 999999999, constants.SYSTEM_ROOT_UID)
+	if err != nil {
+		return err
+	}
+	// 写入到redis
+
+	return svc.Redis.Set(constants.REDIS_SYSTEM_ROOT_TOKEN, systemToken)
 }
